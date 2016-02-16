ead709419e6493d77553de1a9eebcfffe4d169e3
Afton Downey and Brianna Popson <bpopson@gmail.com>
2016-02-15 16:08:28 -0800

    create two more passing specs for hours

--- src/ClockAngleCalculator.php
+++ src/ClockAngleCalculator.php
@@ -7,7 +7,10 @@
             if ($input_hour <= 6){
               return $input_hour * 30;
             }
-
+            elseif ($input_hour > 6 && $input_hour < 12) {
+              return ($input_hour - 6) * 30;
+            }
+           return 0;
         }
     }
 ?>
--- tests/ClockAngleCalculatorTest.php
+++ tests/ClockAngleCalculatorTest.php
@@ -16,5 +16,31 @@
             //Assert
             $this->assertEquals(30, $result);
           }
+
+          function test_ClockAngleCalculator_overSix()
+          {
+            //Arrange
+            $test_ClockAngleCalculator = new ClockAngle;
+            $input = 8;
+
+            //Act
+            $result = $test_ClockAngleCalculator->makeClockAngle($input);
+
+            //Assert
+            $this->assertEquals(60, $result);
+          }
+
+          function test_ClockAngleCalculator_twelve()
+          {
+            //Arrange
+            $test_ClockAngleCalculator = new ClockAngle;
+            $input = 12;
+
+            //Act
+            $result = $test_ClockAngleCalculator->makeClockAngle($input);
+
+            //Assert
+            $this->assertEquals(0, $result);
+          }
     }
 ?>
